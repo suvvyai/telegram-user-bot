9404e1c63caaf4ac68229e85b13d0d74003fbe79
roman mac <barabum@duck.com>
2023-11-28 21:13:22 +0300

    :recycle: refactor: switch to AsyncSuvvyAPIWrapper and improve log message

--- telegram_user_bot/handlers/messages.py
+++ telegram_user_bot/handlers/messages.py
@@ -1,17 +1,19 @@
 from loguru import logger
 from pyrogram import Client, enums
 from pyrogram.types import Message
-from suvvyapi import Message as SuvvyMessage
-from suvvyapi import SuvvyAPIWrapper
+from suvvyapi import Message as SuvvyMessage, AsyncSuvvyAPIWrapper
 
 from telegram_user_bot.config import Config
 
 
 async def on_message(client: Client, message: Message, config: Config) -> None:
-    logger.info("Received message from {message_sender}", message_sender=f"{message.from_user.first_name} {message.from_user.last_name if message.from_user.last_name else ' '}".strip())
+    logger.info("Received message from {message_sender} - \"{text}\"",
+                message_sender=f"{message.from_user.first_name} {message.from_user.last_name if message.from_user.last_name else ' '}".strip(),
+                text=message.text
+                )
 
     await client.send_chat_action(message.chat.id, enums.ChatAction.TYPING)
-    suvvy = SuvvyAPIWrapper(config.suvvy_api_key)
+    suvvy = AsyncSuvvyAPIWrapper(config.suvvy_api_key)
     response = await suvvy.predict(
         message=SuvvyMessage(text=message.text),
         unique_id=f"suvvyai/telegram-user-bot {message.from_user.id}"
