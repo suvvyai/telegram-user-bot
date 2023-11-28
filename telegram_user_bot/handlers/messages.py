bb9ab1bfd9da5d91b2173ce069604ae4afb2a56d
roman mac <barabum@duck.com>
2023-11-28 20:42:49 +0300

    :recycle: refactor: update message logging to include sender's name

--- telegram_user_bot/handlers/messages.py
+++ telegram_user_bot/handlers/messages.py
@@ -3,6 +3,6 @@ from pyrogram import Client
 from pyrogram.types import Message
 
 
-async def on_message(message: Message, client: Client) -> None:
-    logger.info("Received message from @{message_sender}", message_sender=message.chat.username)
+async def on_message(client: Client, message: Message) -> None:
+    logger.info("Received message from {message_sender}", message_sender=f"{message.from_user.first_name} {message.from_user.last_name if message.from_user.last_name else ' '}".strip())
     pass
