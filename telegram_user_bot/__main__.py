1e0cf4ddeb6b66dfd611e029546c2b556fe5ef62
roman mac <barabum@duck.com>
2023-11-28 20:41:30 +0300

    :recycle: refactor: streamline bot startup and config loading process

--- telegram_user_bot/__main__.py
+++ telegram_user_bot/__main__.py
@@ -1,3 +1,4 @@
+import asyncio
 import json
 
 from pyrogram import Client
@@ -11,14 +12,15 @@ from telegram_user_bot.handlers import add_handlers
 @click.command()
 @click.option("--config", "-c", type=click.Path(exists=True, dir_okay=False, readable=True), default="config.json")
 def main(config: str = "config.json") -> None:
-    config = Config(**json.loads(open(config, "r", encoding='utf-8').read()))
-    logger.success("Loaded config from {config_path}", config_path=config)
-
-    logger.info("Authorizing with given user phone number...")
-    client = Client(name=f"users/{config.phone_number.strip('+ ')}", api_id=config.api_id, api_hash=config.api_hash, phone_number=config.phone_number)
-    logger.success("Successfully authorized!")
+    config_path = config
+    config = Config(**json.loads(open(config_path, "r", encoding='utf-8').read()))
+    logger.success("Loaded config from {config_path}", config_path=config_path)
+    client = Client(name=f"{config.phone_number.strip('+ ')}", api_id=config.api_id, api_hash=config.api_hash,
+                    phone_number=config.phone_number)
 
     add_handlers(client)
+    logger.success("Starting bot...")
+    client.run()
 
 
 if __name__ == '__main__':
