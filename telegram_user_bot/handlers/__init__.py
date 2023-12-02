e0465539888a353262a83dca1ec9ea69846b64a7
roman mac <barabum@duck.com>
2023-12-02 15:00:08 +0300

    :sparkles: feat(handlers): add text filter to message handler

--- telegram_user_bot/handlers/__init__.py
+++ telegram_user_bot/handlers/__init__.py
@@ -1,8 +1,8 @@
-from pyrogram import Client
+from pyrogram import Client, filters
 from pyrogram.handlers import MessageHandler
 
 from telegram_user_bot.handlers.messages import on_message
 
 
 def add_handlers(app: Client):
-    app.add_handler(MessageHandler(on_message))
+    app.add_handler(MessageHandler(on_message, filters.incoming & filters.text))
