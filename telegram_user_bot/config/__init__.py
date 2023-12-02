92852e5b4a394400e3b682e81b90e9dd83ab8423
roman mac <barabum@duck.com>
2023-12-02 15:28:40 +0300

    :bug: fix: add default values for session_name and timeouts in Config

--- telegram_user_bot/config/__init__.py
+++ telegram_user_bot/config/__init__.py
@@ -2,11 +2,11 @@ from pydantic import BaseModel, Field
 
 
 class TimeoutsConfig(BaseModel):
-    before_read_seconds: int = Field(ge=0)
-    before_answer_seconds: int = Field(ge=0)
+    before_read_seconds: int = Field(ge=0, default=0)
+    before_answer_seconds: int = Field(ge=0, default=0)
 
 
 class Config(BaseModel):
-    session_name: str
+    session_name: str = "session"
     suvvy_api_key: str
-    timeouts: TimeoutsConfig
+    timeouts: TimeoutsConfig = TimeoutsConfig()
