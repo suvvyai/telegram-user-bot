efcb3020c3133f31e8d70aaa276691c6c2c05b66
roman mac <barabum@duck.com>
2024-02-05 21:21:40 +0300

    🛠️ style: standardize quotes and import statements

--- telegram_user_bot/handlers/messages.py
+++ telegram_user_bot/handlers/messages.py
@@ -3,7 +3,8 @@ import asyncio
 from loguru import logger
 from pyrogram import Client
 from pyrogram.types import Message
-from suvvyapi import Message as SuvvyMessage, AsyncSuvvyAPIWrapper
+from suvvyapi import AsyncSuvvyAPIWrapper
+from suvvyapi import Message as SuvvyMessage
 from suvvyapi.exceptions.api import HistoryStoppedError
 
 from telegram_user_bot.config import config
@@ -11,9 +12,11 @@ from telegram_user_bot.utils.status import keep_typing
 
 
 async def on_message(client: Client, message: Message) -> None:
-    logger.info("Message received from {user}",
-                user=f"{message.from_user.first_name}"
-                     f"{f' {message.from_user.last_name}' if message.from_user.last_name is not None else ''}")
+    logger.info(
+        "Message received from {user}",
+        user=f"{message.from_user.first_name}"
+        f"{f' {message.from_user.last_name}' if message.from_user.last_name is not None else ''}",
+    )
     logger.info("Message received: {text}", text=message.text)
 
     typing_event = asyncio.Event()
@@ -25,13 +28,15 @@ async def on_message(client: Client, message: Message) -> None:
         response = await suvvy.predict(
             message=SuvvyMessage(text=message.text),
             unique_id=f"suvvyai/telegram-user-bot {message.from_user.id}",
-            raise_if_dialog_stopped=True
+            raise_if_dialog_stopped=True,
         )
         logger.success("Suvvy AI answered: {text}", text=response.actual_response.text)
 
-        logger.info("Waiting for {before_read} + {before_answer} seconds before replying",
-                    before_read=config.timeouts.before_read_seconds,
-                    before_answer=config.timeouts.before_answer_seconds)
+        logger.info(
+            "Waiting for {before_read} + {before_answer} seconds before replying",
+            before_read=config.timeouts.before_read_seconds,
+            before_answer=config.timeouts.before_answer_seconds,
+        )
         await asyncio.sleep(config.timeouts.before_read_seconds)
         await client.read_chat_history(message.chat.id)
         await asyncio.sleep(config.timeouts.before_answer_seconds)
