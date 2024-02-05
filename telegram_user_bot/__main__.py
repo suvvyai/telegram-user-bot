c7a59f813dab6ca87224e90a3ea8b43ad2c60eee
roman mac <barabum@duck.com>
2024-02-05 21:07:27 +0300

    🛠️ fix(main): simplify main function and fix config import

--- telegram_user_bot/__main__.py
+++ telegram_user_bot/__main__.py
@@ -1,30 +1,15 @@
-import asyncio
-import json
-
-from pyrogram_patch import patch
-
-from pyrogram import Client
 from loguru import logger
-import click
+from pyrogram import Client
 
-from telegram_user_bot.config import Config
+from telegram_user_bot.config import config
 from telegram_user_bot.handlers import add_handlers
-from telegram_user_bot.middlewares.value import ParamValueMiddleware
 from telegram_user_bot.utils.log import configure_logger
 
 
-@click.command()
-@click.option("--config", "-c", type=click.Path(exists=True, dir_okay=False, readable=True), default="config.json")
-def main(config: str = "config.json") -> None:
+def main() -> None:
     configure_logger()
-    config_path = config
-    config = Config(**json.loads(open(config_path, "r", encoding='utf-8').read()))
-    logger.success("Loaded config from {config_path}", config_path=config_path)
 
     client = Client(name=config.session_name, device_model="suvvyai/telegram-user-bot", system_version="Github latest")
-
-    patch_manager = patch(client)
-    patch_manager.include_middleware(ParamValueMiddleware(key="config", value=config))
 
     add_handlers(client)
     logger.success("Starting bot...")
