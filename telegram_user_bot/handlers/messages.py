183259d5ade31d1ac8fd186a07d6514a2b3eaf68
roman mac <barabum@duck.com>
2024-03-15 00:47:18 +0300

    🐛 fix: добавил вызов `seek` для аудио потока перед его чтением

--- telegram_user_bot/handlers/messages.py
+++ telegram_user_bot/handlers/messages.py
@@ -57,6 +57,7 @@ async def on_message(client: Client, message: Message) -> None:
         if message.voice is not None:
             logger.info("Caught audio message")
             audio_io = await message.download(file_name="audio.ogg", in_memory=True)
+            audio_io.seek()
             logger.info(magic.from_buffer(audio_io.read(), mime=True))
             logger.info(message.voice.mime_type)
             if isinstance(audio_io, BytesIO):
