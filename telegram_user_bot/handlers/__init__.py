bda5c02c37a4197651cf4926809291557a35df8d
roman mac <barabum@duck.com>
2024-03-14 11:15:55 +0300

    ✨ feat(обработчики): добавлена поддержка аудио сообщений

--- telegram_user_bot/handlers/__init__.py
+++ telegram_user_bot/handlers/__init__.py
@@ -5,4 +5,4 @@ from telegram_user_bot.handlers.messages import on_message
 
 
 def add_handlers(app: Client) -> None:
-    app.add_handler(MessageHandler(on_message, filters.incoming & filters.text))
+    app.add_handler(MessageHandler(on_message, filters.incoming & (filters.text | filters.audio)))
